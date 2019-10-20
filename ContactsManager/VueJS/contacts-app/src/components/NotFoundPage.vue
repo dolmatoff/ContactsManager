e799614f37294f05e3e76dc76eaddfa6c11cacc2
dolmatoff <jucy7art67@gmail.com>
2019-10-21 01:58:37 +0300

    Change UI - Add 404 page, fix api-methods handling

---
+++ ContactsManager/VueJS/contacts-app/src/components/NotFoundPage.vue
@@ -0,0 +1,8 @@
+﻿<template>
+    <v-alert color="primary"
+             dark
+             icon="mdi-vuetify"
+             border="left"
+             prominent
+             class="mx-auto"> Page not found</v-alert>
+</template>
